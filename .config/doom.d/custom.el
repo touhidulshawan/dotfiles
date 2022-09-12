c7b39d4150fac418d204bc6e68b6a5316e74d0ef
Touhidul Shawan <touhidulshawan@gmail.com>
2022-09-12 18:34:29 +0600

    add config files of doomemacs

---
+++ .config/doom.d/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(xah-fly-keys lua-mode fish-mode dart-mode lsp-mode lsp-dart lsp-treemacs flycheck company lsp-ui company hover))
+ '(warning-suppress-types '((lsp-mode) (lsp-mode) (defvaralias))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
