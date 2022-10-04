bd1ef15816964d2d735883b009c4de7e5cd37bba
Touhidul Shawan <touhidulshawan@gmail.com>
2022-10-04 12:41:37 +0600

    using builtin format

--- .config/nvim/lua/shawan/maps/format.lua
+++ .config/nvim/lua/shawan/maps/format.lua
@@ -2,4 +2,6 @@ local map = vim.api.nvim_set_keymap
 
 local option = { noremap = true, silent = true }
 
-map("n", "<leader>ft", "<cmd>Format<cr>", option)
+map("n", "<leader>ft", "<cmd>lua vim.lsp.buf.format()<cr>", option)
+
+vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
