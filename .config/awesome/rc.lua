14d5c890a6a19153796f9f5b3e84ffb16b67cfe9
Touhidul Shawan <touhidulshawan@gmail.com>
2022-11-05 01:28:46 +0600

    remove keybindings

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -205,12 +205,6 @@ globalkeys = mytable.join(-- Destroy all notifications
 	awful.key({ modkey }, "Left", awful.tag.viewprev, { description = "view previous", group = "tag" }),
 	awful.key({ modkey }, "Right", awful.tag.viewnext, { description = "view next", group = "tag" }),
 	awful.key({ modkey }, "Escape", awful.tag.history.restore, { description = "go back", group = "tag" }),
-	awful.key({ altkey }, "Left", function()
-		lain.util.tag_view_nonempty(-1)
-	end, { description = "view  previous nonempty", group = "tag" }),
-	awful.key({ altkey }, "Right", function()
-		lain.util.tag_view_nonempty(1)
-	end, { description = "view  previous nonempty", group = "tag" }), -- Default client focus
 	awful.key({ altkey }, "j", function()
 		awful.client.focus.byidx(1)
 	end, { description = "focus next by index", group = "client" }),
