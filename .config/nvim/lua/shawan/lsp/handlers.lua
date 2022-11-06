eb436ac7149b2a5cd4cc138f713457a3c1aacf1e
Touhidul Shawan <touhidulshawan@gmail.com>
2022-11-06 23:30:57 +0600

    fix issues

--- .config/nvim/lua/shawan/lsp/handlers.lua
+++ .config/nvim/lua/shawan/lsp/handlers.lua
@@ -45,7 +45,7 @@ end
 
 local function lsp_highlight_document(client)
 	-- Set autocommands conditional on server_capabilities
-	if client.resolved_capabilities.document_highlight then
+	if client.server_capabilities.document_highlight then
 		vim.api.nvim_exec(
 			[[
       augroup lsp_document_highlight
@@ -83,12 +83,12 @@ local function lsp_keymaps(bufnr)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>dg", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-	vim.cmd([[ command! Format execute 'lua vim.lsp.buf.formatting()' ]])
+	vim.cmd([[ command! Format execute 'lua vim.lsp.buf.format {async = true}' ]])
 end
 
 M.on_attach = function(client, bufnr)
 	if client.name == "tsserver" then
-		client.resolved_capabilities.document_formatting = false
+		client.server_capabilities.documentFormattingProvider = false
 	end
 	lsp_keymaps(bufnr)
 	lsp_highlight_document(client)
@@ -102,6 +102,5 @@ if not status_ok then
 end
 
 M.capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
-
 
 return M
--- .config/nvim/lua/shawan/maps/format.lua
+++ .config/nvim/lua/shawan/maps/format.lua
@@ -2,6 +2,6 @@ local map = vim.api.nvim_set_keymap
 
 local option = { noremap = true, silent = true }
 
-map("n", "<leader>ft", "<cmd>lua vim.lsp.buf.format()<cr>", option)
+map("n", "<leader>ft", "<cmd>lua vim.lsp.buf.format{async = true}<cr>", option)
 
-vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
+vim.cmd([[autocmd BufWritePre <buffer> lua vim.lsp.buf.format {async = true}]])
