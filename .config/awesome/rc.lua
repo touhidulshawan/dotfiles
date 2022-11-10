0fc5a5a97ef8c4fb028e2fdc6cb9ce51ae6603df
Touhidul Shawan <touhidulshawan@gmail.com>
2022-11-10 20:22:39 +0600

    add Gcr-prompter to floating

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -582,6 +582,7 @@ awful.rules.rules = {
 				"Wpa_gui",
 				"veromix",
 				"xtightvncviewer",
+				"Gcr-prompter"
 			},
 			-- Note that the name property shown in xprop might be set slightly after creation of the client
 			-- and the name shown there might not match defined rules here.
