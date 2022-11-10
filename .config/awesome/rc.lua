1fdd55a87daef7948a7ad2ce815eea03fdb6c5c8
Touhidul Shawan <touhidulshawan@gmail.com>
2022-11-10 20:56:00 +0600

    minor edit

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -586,7 +586,8 @@ awful.rules.rules = {
 				"Wpa_gui",
 				"veromix",
 				"xtightvncviewer",
-				"Gcr-prompter"
+				"Gcr-prompter",
+				"Lxpolkit"
 			},
 			-- Note that the name property shown in xprop might be set slightly after creation of the client
 			-- and the name shown there might not match defined rules here.
