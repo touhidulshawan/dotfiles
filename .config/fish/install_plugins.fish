d4c1bf51128b6d0020f4d79dc816a5dbeea428c4
Touhidul Shawan <touhidulshawan@gmail.com>
2022-11-13 23:20:46 +0600

    update gitnow

--- .config/fish/install_plugins.fish
+++ .config/fish/install_plugins.fish
@@ -1,10 +1,8 @@
 #!/usr/bin/fish
 
-sudo pacman -S fisher
-
 fisher install jorgebucaran/nvm.fish
 fisher install jethrokuan/z
-fisher install joseluisq/gitnow@2.8.0
+fisher install joseluisq/gitnow@2.10.0
 fisher install PatrickF1/fzf.fish
 fisher install jorgebucaran/autopair.fish
 fisher install oh-my-fish/plugin-pbcopy
