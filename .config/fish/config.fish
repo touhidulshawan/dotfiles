fd736e3afcd12124b3f71b8d50b86d4af9642add
Touhidul Shawan <touhidulshawan@gmail.com>
2023-03-12 16:30:30 +0600

    add new function

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -136,6 +136,14 @@ function ff
     end
 end
 
+# function to query zoxide and cd to it
+function zz
+    set folderPath (zoxide query -l | fzf --reverse)
+    if string length -q -- $folderPath
+        cd $folderPath
+    end
+end
+
 # open file
 function open
     set fileName (fd . -Ht f  | fzf)
