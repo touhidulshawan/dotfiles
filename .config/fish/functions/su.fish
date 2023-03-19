4cb9e0de1e695d2951d57788af1791aee7cf2d53
Touhidul Shawan <touhidulshawan@gmail.com>
2023-03-20 00:19:36 +0600

    add fish function

---
+++ .config/fish/functions/su.fish
@@ -0,0 +1,12 @@
+#!/usr/bin/fish
+
+# Author: Touhidul Shawan
+# Date Created: 20-03-2023
+# Date Modified: 20-03-2023
+
+# Description
+# If su starts with Bash because Bash is the target user's (root if no username is provided) default shell, one can define a function to redirect it to fish whatever the user's shell
+
+function su
+    command su --shell=/usr/bin/fish $argv
+end
