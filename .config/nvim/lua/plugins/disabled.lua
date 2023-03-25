8e9b3475c79e9e5d3287424a663085129c72c013
Touhidul Shawan <touhidulshawan@gmail.com>
2023-03-25 20:17:31 +0600

    disable plugin

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,6 +1,5 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false },
-
-  -- { "folke/noice.nvim", enabled = false },
-  -- { "rcarriga/nvim-notify", enabled = false },
+  { "folke/noice.nvim", enabled = false },
+  { "rcarriga/nvim-notify", enabled = false },
 }
