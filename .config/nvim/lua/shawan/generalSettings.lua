230fa76b29f5f446ff22de3bc26dd624a5b8f28d
Touhidul Shawan <touhidulshawan@gmail.com>
2023-05-13 18:27:44 +0600

    modify setting

--- .config/nvim/lua/shawan/generalSettings.lua
+++ .config/nvim/lua/shawan/generalSettings.lua
@@ -1,45 +1,30 @@
-vim.opt.backup = false
-vim.opt.exrc = true
-vim.opt.termguicolors = true
-vim.opt.shell = "/usr/bin/fish"
-vim.opt.hlsearch = false
-vim.opt.hidden = true
-vim.opt.swapfile = false
-vim.opt.number = true
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.nu = true
 vim.opt.relativenumber = true
-vim.opt.errorbells = true
-vim.opt.spell = true
-vim.opt.wildmenu = true
-vim.opt.lazyredraw = true
-vim.opt.showmatch = true
-vim.opt.incsearch = true
-vim.opt.foldenable = true
-vim.opt.foldlevelstart = 10
-vim.opt.foldnestmax = 10
-vim.opt.foldmethod = "indent"
-vim.opt.smarttab = true
-vim.opt.cindent = true
+
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-vim.opt.smartcase = true
-vim.opt.smartindent = true
-vim.opt.scrolloff = 8
-vim.opt.colorcolumn = "80"
-vim.opt.textwidth = 80
-vim.opt.signcolumn = "yes"
-vim.opt.encoding = "utf-8"
-vim.opt.clipboard = "unnamedplus"
+
 vim.opt.wrap = false
-vim.opt.spelllang = "en"
-vim.opt.numberwidth = 5
-vim.opt.linebreak = true
-vim.opt.backspace = {"indent", "eol", "start"}
-vim.opt.completeopt = "menuone,noselect"
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-vim.opt.conceallevel = 0
-vim.opt.pumheight = 10
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
 vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
 vim.cmd("autocmd InsertEnter * norm zz")
