89825a37822db3f7d44fc2fda16a37d1f4077ab3
Touhidul Shawan <touhidulshawan@gmail.com>
2023-05-29 20:17:38 +0600

    replace paru with yay

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -115,8 +115,8 @@ alias jctl="journalctl -p 3 -xb"
 # unlock pacman
 alias unlock="sudo rm /var/lib/pacman/db.lck"
 # search package and install from pacman
-alias add="yay -Slq | fzf --multi --preview 'paru -Si {1}' | xargs -ro paru -S"
-alias remove="yay -Qq | fzf --multi --preview 'paru -Qi {1}' | xargs -ro sudo paru -Rns"
+alias add="yay -Slq | fzf --multi --preview 'yay -Si {1}' | xargs -ro yay -S"
+alias remove="yay -Qq | fzf --multi --preview 'yay -Qi {1}' | xargs -ro sudo yay -Rns"
 
 # alias to fix tryhackme machine website issues
 alias nmfix="sudo ip link set dev nm-bridge mtu 1200"
