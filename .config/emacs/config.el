0e5240766af4ea63d5fc1f093ddb4b5a880243b3
Touhidul Shawan <touhidulshawan@gmail.com>
2023-07-17 15:09:52 +0600

    add corfu [completion]

--- .config/emacs/config.el
+++ .config/emacs/config.el
@@ -14,10 +14,6 @@
   (setq use-package-always-ensure t
 	use-package-expand-minimally t))
 
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-
 (set-face-attribute 'default nil
   :font "JetBrainsMono Nerd Font"
   :height 105
@@ -41,14 +37,32 @@
 (global-display-line-numbers-mode 1)
 (setq display-line-numbers-type 'relative)
 
+(prefer-coding-system 'utf-8)
+
+(fset 'yes-or-no-p 'y-or-n-p)
+ ;; use primary as clipboard
+ (setq-default x-select-enable-primary t)
+ ;; avoid leaving a gap between the frame and the screen
+ (setq-default frame-resize-pixelwise t)
+
+;; Vim like scrolling
+ (setq scroll-step            1
+       scroll-conservatively  10000
+       next-screen-context-lines 5
+       ;; move by logical lines rather than visual lines (better for macros)
+       line-move-visual nil)
+
+(customize-set-value 'recentf-make-menu-items 150)
+(customize-set-value 'recentf-make-saved-items 150)
+
 (load-theme 'gruvbox-dark-hard t)
 
 (use-package evil
      :init
-     (setq evil-want-integration t)
-     (setq evil-want-keybinding nil)
-     (setq evil-vsplit-window-right t)
-     (setq evil-split-window-below t)
+     (setq evil-want-integration t
+           evil-want-keybinding nil
+           evil-vsplit-window-right t
+           evil-split-window-below t)
      (evil-mode))
 
    (use-package evil-collection
@@ -60,18 +74,19 @@
    ;; jj to escape to normal mode
 (evil-escape-mode)
 (setq-default evil-escape-key-sequence "jj"
-	      evil-escape-delay 0.2
+              evil-escape-delay 0.2
               evil-escape-inhibit-functions '(evil-visual-state-p))
 
 (use-package dashboard
   :init
   (setq dashboard-set-heading-icons t
-	dashboard-set-file-icons t
-	dashboard-startup-banner "~/.config/emacs/gruvbox.png"
-	dashboard-center-content nil
-	dashboard-items '((recents . 8)))
-	:config
-	(dashboard-setup-startup-hook))
+        dashboard-set-file-icons t
+        dashboard-display-icons-p t
+        dashboard-startup-banner "~/.config/emacs/gruvbox.png"
+        dashboard-center-content nil
+        dashboard-items '((recents . 8)))
+        :config
+        (dashboard-setup-startup-hook))
 (setq initial-buffer-choice (lambda () (get-buffer-create "*dashboard*")))
 (setq doom-fallback-buffer-name "*dashboard*")
 
@@ -138,3 +153,30 @@
   "j" '(avy-goto-word-0 :wk "Go to word")
   "l" '(avy-goto-line :wk "Go to line"))
 )
+
+(use-package corfu
+  ;; Optional customizations
+  :custom
+  (corfu-cycle t)                 ; Allows cycling through candidates
+  (corfu-auto t)                  ; Enable auto completion
+  (corfu-auto-prefix 2)
+  (corfu-auto-delay 0.0)
+  (corfu-popupinfo-delay '(0.5 . 0.2))
+  (corfu-preview-current 'insert) ; Do not preview current candidate
+  (corfu-preselect 'prompt)
+  (corfu-on-exact-match nil)      ; Don't auto expand tempel snippets
+
+  ;; Optionally use TAB for cycling, default is `corfu-complete'.
+  :bind (:map corfu-map
+              ("M-SPC"      . corfu-insert-separator)
+              ("TAB"        . corfu-next)
+              ([tab]        . corfu-next)
+              ("S-TAB"      . corfu-previous)
+              ([backtab]    . corfu-previous)
+              ("S-<return>" . corfu-insert)
+              ("RET"        . nil))
+
+  :init
+  (global-corfu-mode)
+  (corfu-history-mode)
+  (corfu-popupinfo-mode)) ; Popup completion info
