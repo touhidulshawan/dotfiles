6ef4684c1fd57a239276c4e45763239303f92d60
Touhidul Shawan <touhidulshawan@gmail.com>
2023-07-17 22:25:38 +0600

    finish up
    emacs setup for org file

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -2,16 +2,3 @@
  (expand-file-name
   "config.org"
   user-emacs-directory))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(sudo-edit pipenv rainbow-mode diminish eglot flycheck doom-modeline evil-surround evil-commentary marginalia orderless vertico quelpa-use-package quelpa yasnippet cape org-modern toc-org avy ## which-key nerd-icons-ivy-rich nerd-icons-ibuffer nerd-icons-completion nerd-icons-dired dashboard nerd-icons evil-nerd-commenter gruvbox-theme use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
