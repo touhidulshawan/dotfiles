b4df059a5eefb3b721c794b42e3c8e4a3b96cde1
Touhidul Shawan <touhidulshawan@gmail.com>
2023-08-15 21:45:20 +0600

    add abbr in seperate file

---
+++ .config/fish/abbr.fish
@@ -0,0 +1,24 @@
+abbr --add  berry 'yarn set version berry' 
+abbr --add  ncm ncmpcpp
+abbr --add  pbc pbcopy
+abbr --add  pbp pbpaste
+abbr --add  py python
+abbr --add  blt bluetoothctl
+abbr --add  bl bluetooth
+abbr --add  bm bashmount
+abbr --add  e exit
+abbr --add  cl clear
+abbr --add  bt btop
+abbr --add  n nvim
+abbr --add  up 'sudo ip link set dev wlan0 up'
+abbr --add  down 'sudo ip link set dev wlan0 down'
+abbr --add  gpkg 'npm install -g eslint prettier prettier-plugin-toml yarn'
+abbr --add  st startx
+abbr --add  cn config
+abbr --add  tl trash-list
+abbr --add  rr trash-restore
+abbr --add  td trash-empty
+abbr --add  dcom docker-compose
+abbr --add  oc 'ouch compress'
+abbr --add  od 'ouch decompress'
+abbr --add  sd ssh-add
