f09d129503105030d216c0ab1b21a8a9d845f964
Touhidul Shawan <touhidulshawan@gmail.com>
2023-08-30 06:18:21 +0600

    minor edit

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -7,6 +7,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("7a3f3282c4ce1edc3387deb6f72ed31026ab7dfdf4c25ddcfad9008d2a5a5574" "d445c7b530713eac282ecdeea07a8fa59692c83045bf84dd112dd738c7bcad1d" "4363ac3323e57147141341a629a19f1398ea4c0b25c79a6661f20ffc44fdd2cb" default))
  '(package-selected-packages
    '(yasnippet-snippets which-key vertico tree-sitter-langs toc-org sudo-edit spinner rainbow-mode quelpa-use-package pyvenv project pkg-info org-modern orderless nerd-icons-ivy-rich nerd-icons-ibuffer nerd-icons-dired nerd-icons-completion markdown-mode marginalia magit lv lua-mode load-env-vars ligature language-id kind-icon key-chord jsonrpc inheritenv ht gruvbox-theme general gcmh fish-mode external-completion evil-surround evil-org evil-nerd-commenter evil-commentary evil-collection elisp-autofmt eldoc doom-modeline diminish dashboard corfu centered-cursor-mode cape-yasnippet cape avy affe)))
 (custom-set-faces
