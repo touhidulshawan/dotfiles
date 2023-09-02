f90f7f9839f47cd4197ae58c983a560d980fdbce
Touhidul Shawan <touhidulshawan@gmail.com>
2023-09-03 05:24:08 +0600

    minor edit

--- .config/nvim/after/plugin/trouble.lua
+++ .config/nvim/after/plugin/trouble.lua
@@ -1,6 +1,6 @@
 local option = {silent = true, noremap = true}
 
-vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>", option)
+vim.keymap.set("n", "<leader>xt", "<cmd>TroubleToggle<cr>", option)
 vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", option)
 vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>", option)
 vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", option)
