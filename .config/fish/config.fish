b3b38c354fe5d548ee0754b14df2fce5267e562f
Touhidul Shawan <touhidulshawan@gmail.com>
2023-09-15 00:07:14 +0600

    add alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -72,6 +72,9 @@ alias grep='grep --color=auto'
 alias egrep='egrep --color=auto'
 alias fgrep='fgrep --color=auto'
 
+# batgrep
+alias rg=batgrep
+
 # storage
 alias du='du -sh'
 alias ncdu='ncdu --color dark'
