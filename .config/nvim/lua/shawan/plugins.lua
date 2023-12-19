c1534c3dc3832b3174b25ebbd3ac5f674a672b2e
Touhidul Shawan <touhidulshawan@gmail.com>
2023-12-19 15:46:34 +0600

    update & fix LuaSnip plugin update error

--- .config/nvim/lua/shawan/plugins.lua
+++ .config/nvim/lua/shawan/plugins.lua
@@ -39,7 +39,13 @@ local plugins   = {
             { 'hrsh7th/cmp-nvim-lua' },
 
             -- Snippets
-            { 'L3MON4D3/LuaSnip' },
+            {
+                "L3MON4D3/LuaSnip",
+                -- follow latest release.
+                version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+                -- install jsregexp (optional!).
+                build = "make install_jsregexp"
+            },
             { 'rafamadriz/friendly-snippets' },
         }
     },
